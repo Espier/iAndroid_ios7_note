487c56f13109a15a00a49fc8998ed717e8fd1146
lemon <lemon@espier.org>
2013-08-28 15:26:52 +0800

    init project

---
+++ src/org/espier/note7/EspierNote7.java
@@ -0,0 +1,15 @@
+package org.espier.note7;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EspierNote7 extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
